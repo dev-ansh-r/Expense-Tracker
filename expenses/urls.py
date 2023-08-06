4c4e694960aa3d720b2359529031c4a8acd12b08
dev-ansh-r <rdevanshshukla26@gmail.com>
2023-08-06 14:29:26 +0530

    CI CD

--- expenses/urls.py
+++ expenses/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('expense/',views.expense_list, name='expense-list'),
-    path('expense/<int:pk>/update/', views.expense_update, name='expense-update'),
-    path('expense/<int:pk>/delete/', views.expense_delete, name='expense-delete'),
+    path('',views.expense_list, name='expense-list'),
+    path('  <int:pk>/update/', views.expense_update, name='expense-update'),
+    path('<int:pk>/delete/', views.expense_delete, name='expense-delete'),
 ]
--- finance_app/settings.py
+++ finance_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-vj30a_(rir32y%38g9jz*u@)iga=78s%6!%jfob-vm7d1_h$x4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['expensestodoist-production.up.railway.app']
+ALLOWED_HOSTS = ['expensestodoist-production.up.railway.app','localhost']
 
 # Application definition
 
@@ -124,7 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
