8293ac4dbb4b4e786ab75661a58cea3e3d84b372
dev-ansh-r <rdevanshshukla26@gmail.com>
2023-08-07 18:31:32 +0530

    retry 5

--- finance_app/settings.py
+++ finance_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-vj30a_(rir32y%38g9jz*u@)iga=78s%6!%jfob-vm7d1_h$x4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['expensestodoist-production.up.railway.app','localhost','expense-tracker-production-a000.up.railway.app']
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static'
 STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
 # Default primary key field type
